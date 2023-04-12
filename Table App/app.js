75373b638438d32721d2737ffb69c6e89d14e154
Mukhammadkarim's MacBook Pro <macbook@Muhammadkarims-MacBook-Air.local>
2023-04-13 00:11:00 +0500

    Completed

--- Table App/app.js
+++ Table App/app.js
@@ -15,20 +15,20 @@ tableBody.innerHTML = rowContainer;
 btn.addEventListener("click", addRow);
 // Functions
 
-let s = 0;
+let num = 0;
 
 function addRow() {
 	let lastRow = tableBody.lastElementChild;
 	const inpValue = lastRow.getElementsByTagName("input");
 	for (let i = 0; i < inpValue.length; i++) {
 		if (!inpValue[i].value) {
-			s++;
+			num++;
 			alert("Please do'nt skip any input!");
 			break;
 		}
 	}
-	if (s === 0) {
+	if (num === 0) {
 		tableBody.innerHTML += rowContainer;
 	}
-	s = 0;
+	num = 0;
 }
