f494a25ace7fb0d1335a0a52701a77bc6f050785
Mukhammadkarim's MacBook Pro <macbook@Muhammadkarims-MacBook-Air.local>
2023-04-12 15:02:23 +0500

    Bubble Comleted

---
+++ Circle Bubble/app.js
@@ -0,0 +1,16 @@
+document.addEventListener('click', function newBubble(event) {
+	const container = document.createElement('div');
+	container.classList.add('cont');
+	// Random size:
+	const size = Math.floor(Math.random() * (200 - 25) + 25);
+	// Random color:
+	const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+
+	container.style.backgroundColor = randomColor;
+	container.style.width = `${size}px`;
+	container.style.height = `${size}px`;
+	container.style.top = `${event.clientY - (size / 2)}px`;
+	container.style.left = `${event.clientX - (size / 2)}px`;
+
+	document.body.appendChild(container)
+});
