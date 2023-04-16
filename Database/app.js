b136313613f932d148812f32ff960bcb0b942e01
Mukhammadkarim's MacBook Pro <macbook@Muhammadkarims-MacBook-Air.local>
2023-04-16 16:44:25 +0500

    Just pending

--- Database/app.js
+++ Database/app.js
@@ -1,3 +1,29 @@
-import data from './data.js';
+import data from "./data.js";
+// Selectors
+const tabContainer = document.querySelector(".tableContainer");
+const headContainer = document.querySelector(".headCont");
+const bodyContainer = document.querySelector(".bodyCont");
 
-console.log(data, Object.keys(data[0]));
+// Functions
+function generateTh() {
+  const tr = document.createElement("tr");
+  headContainer.appendChild(tr);
+  const keys = Object.keys(data[0]);
+  for (let i = 0; i < keys.length; i++) {
+    tr.innerHTML += `<th>${keys[i]}</th>`;
+  }
+}
+
+function generateTd() {
+  for (let x = 0; x < data.length; x++) {
+    const tr = document.createElement("tr");
+    const values = Object.values(data[x]);
+    bodyContainer.append(tr);
+    for (let i = 0; i < values.length; i++) {
+      tr.innerHTML += `<td>${values[i]}</td>`
+    }
+  }
+}
+
+generateTh();
+generateTd();
